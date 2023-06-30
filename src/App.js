94f0054cdcf49e136a94088209a31d922e2040a0
kumagi8 <123293329+kumagi8@users.noreply.github.com>
2023-06-30 23:34:33 +0530

    qwertyuio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
      
-      <BrowserRouter>
+      <BrowserRouter basename='fe'>
       <Routes>
         <Route path="/" element={ <Home/>} />
       </Routes>
