7bdf4cc2ba5b004bef665be209e5df52aa1edef5
kumagi8 <123293329+kumagi8@users.noreply.github.com>
2023-07-04 00:03:16 +0530

    intro updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
   return (
     <>
     <header className="main-header">
-      <div className="what">Frontend Developer</div>
+      <div className="what">Kumar,Girish</div>
       <div className="navigate">
       <nav>
-        <Link to="/">Home</Link> 
-        <Link to="/">Skills</Link> 
+        <Link to="/">Frontend Developer</Link> 
+        {/* <Link to="/">Skills</Link> 
         <Link to="/">Projects</Link> 
-        <Link to="/">Contact</Link> 
+        <Link to="/">Contact</Link>  */}
       </nav>
       </div>
     
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -4,14 +4,15 @@ function Introduction(){
     return (
         <div className="intro-section">
             <div className="profile">
-                <div className="name">
-                <h1>Hi,</h1>
-                <h1>Girish Kumar</h1>
-                </div>
-            </div>
-            <div className="profile">
                <img className="profile-photo" src={profile} alt="profile" />
             </div>
+            <div className="profile">
+                <div className="name">
+                <h1>Welcome to My Portfolio</h1>
+                <h1>Hi, I'm a frontend developer passionate about creating user-friendly and visually appealing websites.</h1>
+                </div>
+            </div>
+            
         </div>
     )
 }
